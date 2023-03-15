33470b0dfee267e540d29083f06e03d1bbbca0f2
Afnan <55242810+Afnankhn@users.noreply.github.com>
2023-03-15 09:30:47 +0900

    Add files via upload

---
+++ visulaize_feature_maps.py
@@ -0,0 +1,46 @@
+
+
+
+    
+# visualize feature maps output from each block in the vgg model
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg16 import preprocess_input
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import Model
+from matplotlib import pyplot
+from numpy import expand_dims
+# load the model
+model = VGG16()
+# redefine model to output right after the first hidden layer
+ixs = [2, 5, 9, 13, 17]
+outputs = [model.layers[i].output for i in ixs]
+model = Model(inputs=model.inputs, outputs=outputs)
+# load the image with the required shape
+img = load_img('test.jpeg', target_size=(224, 224))
+# convert the image to an array
+img = img_to_array(img)
+# expand dimensions so that it represents a single 'sample'
+img = expand_dims(img, axis=0)
+# prepare the image (e.g. scale pixel values for the vgg)
+img = preprocess_input(img)
+# get feature map for first hidden layer
+feature_maps = model.predict(img)
+# plot the output from each block
+square = 8
+img_n=1
+
+for fmap in feature_maps:
+    ix= 1
+    for r in range(square):
+        for c in range(square):
+            ax=pyplot.subplot(square, square, ix)
+            ax.set_xticks([])
+            ax.set_yticks([])
+            pyplot.imshow(fmap[0,:,:,ix-1], cmap='inferno')
+            ix+=1
+    pyplot.savefig(str(img_n) +".png", dpi=600)
+    pyplot.show()
+    img_n+=1
+    
+    
